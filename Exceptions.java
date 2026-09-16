5a470f894222d03de6d2948db033f64dc1babe92
viv08bhagwat-coder <viv08.bhagwat@gmail.com>
2026-09-16 11:08:55 +0530

    Update Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -1,18 +1,20 @@
-public class Exceptions {
-    public static void main(String[] args) {
-        try {
-            int a = 10 / 0;
-        } catch (ArithmeticException e) {
-            System.out.println("ArithmeticException");
-        }
-
-        try {
-            int[] arr = {1, 2, 3};
-            int b = arr[5];
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBoundsException");
+public class Exceptions { 
+    public static void main(String[] args) { 
+        try { 
+            int a = 10; 
+            int b = 0; 
+            int result = a / b; 
+            System.out.println(result); 
+            
+            int[] numbers = {1, 2, 3}; 
+            System.out.println(numbers[1]); 
+        } catch (ArithmeticException e) { 
+            System.out.println("Math error"); 
+        } catch (ArrayIndexOutOfBoundsException e) { 
+            System.out.println("Array error"); 
         } finally {
-            System.out.println("Finally");
+            System.out.println("Done");
         }
-    }
+    } 
 }
+
